7ab098cab1897616ad005433f688aba24e80dbc7
morishitter <morishitter@gmail.com>
2016-07-06 10:40:26 +0900

    first implementation

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+var postcss = require('postcss')
+
 module.exports = function () {
+  return function (root) {
+    root.walkDecls(function (decl) {
+      if (decl.value === 'currentcolor' || decl.value === 'current-color') {
+        decl.value = 'currentColor'
+      }
+    })
+
+    return root
+  }
 }
