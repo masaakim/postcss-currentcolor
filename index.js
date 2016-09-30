8346e3e74472acf3de5ea6e74f995abddf060773
Aleksey Shvayka <shvaikalesh@gmail.com>
2016-09-30 21:03:19 +0300

    call `replace` unconditionally

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ var postcss = require('postcss')
 module.exports = postcss.plugin('postcss-currentcolor', function() {
   return function (root) {
     root.walkDecls(function (decl) {
-      if (decl.value.match(/currentcolor/)) {
-        decl.value = decl.value.replace(/currentcolor/g, 'currentColor')
-      } else if (decl.value.match(/current-color/)) {
-        decl.value = decl.value.replace(/current-color/g, 'currentColor')
-      }
+      decl.value = decl.value.replace(/\bcurrent-?color\b/g, 'currentColor')
     })
-
-    return root
   }
 })
